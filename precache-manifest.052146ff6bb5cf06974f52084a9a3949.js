b34d5381223397284e88317b59358a423d9bc892
Code Sandbox <hello@codesandbox.io>
2023-08-10 04:00:52 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.052146ff6bb5cf06974f52084a9a3949.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af3ed56c03d1748bd1d5b8c081670749",
+    "url": "https://laurenbmcguire.github.io/csb-yfhznf/index.html"
+  },
+  {
+    "revision": "a1e15ade2fe17b152e72",
+    "url": "https://laurenbmcguire.github.io/csb-yfhznf/static/css/2.e4ecef63.chunk.css"
+  },
+  {
+    "revision": "39227f6539bdf7f5cd15",
+    "url": "https://laurenbmcguire.github.io/csb-yfhznf/static/css/main.e60354ee.chunk.css"
+  },
+  {
+    "revision": "a1e15ade2fe17b152e72",
+    "url": "https://laurenbmcguire.github.io/csb-yfhznf/static/js/2.099fd6e2.chunk.js"
+  },
+  {
+    "revision": "fb1302d620764e60eaa58680f4998f32",
+    "url": "https://laurenbmcguire.github.io/csb-yfhznf/static/js/2.099fd6e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39227f6539bdf7f5cd15",
+    "url": "https://laurenbmcguire.github.io/csb-yfhznf/static/js/main.1e570f1b.chunk.js"
+  },
+  {
+    "revision": "ad4faec664c5deca744d",
+    "url": "https://laurenbmcguire.github.io/csb-yfhznf/static/js/runtime-main.8e88f1db.js"
+  }
+]);
